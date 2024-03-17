91a27aea31a4d182644103d4bdb5b3fdc4240ccb
Isaac Qadri <vponsve@gmail.com>
2024-03-18 01:11:16 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Call POST http://localhost:3000/api/team/admin 
+    add simple body
+    <br />
+    Example:
+    <code>curl -X POST -H "Content-Type: application/json" -d '{"name": "John"}' http://localhost:3000/api/team/admin</code>
+
+    Works right?
+    <br />
+
+    Now try calling this from docker.
+    Example:
+    <br />
+    http://host.docker.internal:3000/api/team/admin
+
+    <br />
+
+    Does it work for ya?
+  </div>
+</template>
